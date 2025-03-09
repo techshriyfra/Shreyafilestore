aef7fa18457be20011ff5f328bc5c53780730c0b
techshriyfra <techsashif+eufya@gmail.com>
2025-03-09 10:22:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from bot import Bot
 import pyrogram.utils
 
-pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
+pyrogram.utils.MIN_CHANNEL_ID = -1002354118780 #Add Db Channel id Here -10000000000
 
 if __name__ == "__main__":
     Bot().run()
