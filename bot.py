badfdb34cddeac6d0c4e8b3d19ac644f7b33d20b
techshriyfra <techsashif+eufya@gmail.com>
2025-03-09 09:43:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-#(©)Codexbotz @Codeflix_Bots
+#(©)MrGhostsx
 
 from aiohttp import web
 from plugins import web_server
@@ -41,7 +41,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCESUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCESUB_CHANNEL}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/weebs_support for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Tech_Shreyansh")
                 sys.exit()
         if FORCESUB_CHANNEL2:
             try:
@@ -54,7 +54,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCESUB_CHANNEL2 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCESUB_CHANNEL2}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/weebs_support for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Tech_Shreyansh")
                 sys.exit()
         if FORCESUB_CHANNEL3:
             try:
@@ -67,7 +67,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCESUB_CHANNEL3 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCESUB_CHANNEL3}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/weebs_support for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Tech_Shreyansh for support")
                 sys.exit()       
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
@@ -77,21 +77,23 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/weebs_support for support")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Tech_Shreyansh for support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/Animes_X_Hunters")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/Tech_Shreyansh29")
         self.LOGGER(__name__).info(f""" \n\n       
 
-  _____ ____  _____  ______ ______ _      _______   ______   ____ _______ _____ 
- / ____/ __ \|  __ \|  ____|  ____| |    |_   _\ \ / /  _ \ / __ \__   __/ ____|
-| |   | |  | | |  | | |__  | |__  | |      | |  \ V /| |_) | |  | | | | | (___  
-| |   | |  | | |  | |  __| |  __| | |      | |   > < |  _ <| |  | | | |  \___ \ 
-| |___| |__| | |__| | |____| |    | |____ _| |_ / . \| |_) | |__| | | |  ____) |
- \_____\____/|_____/|______|_|    |______|_____/_/ \_\____/ \____/  |_| |_____/ 
-                                                                                
-                                                                                
+ _   .-')    _  .-')               ('-. .-.               .-')    .-') _    
+( '.( OO )_ ( \( -O )             ( OO )  /              ( OO ). (  OO) )   
+ ,--.   ,--.),------.   ,----.    ,--. ,--. .-'),-----. (_)---\_)/     '._  
+ |   `.'   | |   /`. ' '  .-./-') |  | |  |( OO'  .-.  '/    _ | |'--...__) 
+ |         | |  /  | | |  |_( O- )|   .|  |/   |  | |  |\  \:` `. '--.  .--' 
+ |  |'.'|  | |  |_.' | |  | .--, \|       |\_) |  |\|  | '..`''.)   |  |    
+ |  |   |  | |  .  '.'(|  | '. (_/|  .-.  |  \ |  | |  |.-._)   \   |  |    
+ |  |   |  | |  |\  \  |  '--'  | |  | |  |   `'  '-'  '\       /   |  |    
+ `--'   `--' `--' '--'  `------'  `--' `--'     `-----'  `-----'    `--' 
+                                                           
                                           """)
         self.username = usr_bot_me.username
         #web-response
