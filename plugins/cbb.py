6b562d0571551d67987b1c776a40c3b836fb77f1
techshriyfra <techsashif+eufya@gmail.com>
2025-03-09 10:00:16 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ ᴏᴡɴᴇʀ : <a href='tg://user?id={OWNER_ID}'>ᴛᴇᴄʜ ꜱʜʀᴇʏᴀɴꜱʜ</a>\n○ ᴍʏ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/Tech_Shreyansh29'>ᴛᴇᴄʜ ꜱʜʀᴇʏᴀɴꜱʜ</a>\n○ ᴍᴏᴠɪᴇs ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/+Q5pbPuc8lGk3YWQ1'>ᴛᴇᴀᴍ ɴᴇᴛғʟɪx</a>\n○ ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ : <a href='https://t.me/Tech_Shreyansh2'>ᴛᴇᴄʜ ꜱʜʀᴇʏᴀɴꜱʜ</a>\n○ ɢʀᴏᴜᴘ ᴄʜᴀᴛ : <a href='https://t.me/Tech_Shreyansh2'>ᴛᴇᴄʜ ꜱʜʀᴇʏᴀɴꜱʜ</a></b>",
+            text = f"<b> ᴏᴡɴᴇʀ : <a href='tg://user?id={OWNER_ID}'>ᴛᴇᴄʜ ꜱʜʀᴇʏᴀɴꜱʜ</a>\n○ ᴍʏ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/Tech_Shreyansh29'>ᴛᴇᴄʜ ꜱʜʀᴇʏᴀɴꜱʜ</a>\n○ ᴍᴏᴠɪᴇs ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/+Q5pbPuc8lGk3YWQ1'>ᴛᴇᴀᴍ ɴᴇᴛғʟɪx</a>\n○ ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ : <a href='https://t.me/Tech_Shreyansh2'>ᴛᴇᴄʜ ꜱʜʀᴇʏᴀɴꜱʜ</a>\n○ ɢʀᴏᴜᴘ ᴄʜᴀᴛ : <a href='https://t.me/Tech_Shreyansh2'>ᴛᴇᴄʜ ꜱʜʀᴇʏᴀɴꜱʜ</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
